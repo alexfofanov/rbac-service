b297463f9926216cb9c8ca8e21aed3527c5f7d99
Alex Fofanov <alex.fofanov@gmail.com>
2025-12-09 15:12:18 +0300

    В модель пользователя добавлена роль

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.db import models
 
-# from rbac.models import Role
+from rbac.models import Role
 
 
 class UserManager(BaseUserManager):
@@ -39,8 +39,7 @@ class User(AbstractBaseUser):
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-
-    # role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, blank=True)
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Пользователь'
